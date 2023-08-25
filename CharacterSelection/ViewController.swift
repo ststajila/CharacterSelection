9c0ba81b13c19888bdf4ae2818d325fefd60a543
Stanislav <stazilastas@gmail.com>
2023-08-25 09:49:35 -0500

    CharacterSelection 1.0

--- CharacterSelection/ViewController.swift
+++ CharacterSelection/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var characterOutlet: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func firstCharacterAction(_ sender: Any) {
+        characterOutlet.image = UIImage(named: "monkey")
+    }
+    @IBAction func secondCharacterAction(_ sender: Any) {
+        characterOutlet.image = UIImage(named: "monkey2")
+    }
+    @IBAction func thirdCharacterAction(_ sender: Any) {
+        characterOutlet.image = UIImage(named: "monkey3")
+    }
+    @IBAction func firstAccessoryAction(_ sender: Any) {
+    }
+    
+    @IBAction func secondAccessoryAction(_ sender: Any) {
+    }
+    @IBAction func thirdAccessoryAction(_ sender: Any) {
+    }
 }
 
